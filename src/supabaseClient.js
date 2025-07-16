4eab9294c3e3aad306dc7ba9432dcc3d8617a9a0
Roshan Dayananda <118856338+RoshanD15@users.noreply.github.com>
2025-07-15 22:09:39 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tijpxhgdqcwofkaaxqri.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpanB4aGdkcWN3b2ZrYWF4cXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNjU3MTcsImV4cCI6MjA2NjY0MTcxN30.POhN6xC7ydQB51NR-hf_6eHErM0lxpMisOVCJt8pMqk";
-
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
